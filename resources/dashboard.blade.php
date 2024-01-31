9cef3be018d8f96b7c1e0ebc8c063f36e7680c45
Caen De Silva <caen@desilva.se>
2024-01-31 16:51:38 +0100

    Remove negative margin style override
    Can't remember what this was for, and it causes an offset that makes the element not be centered.

--- resources/dashboard.blade.php
+++ resources/dashboard.blade.php
@@ -288,7 +288,7 @@
                             There are no media files yet. Why not add some?
                         </div>
                     @else
-                       <div class="container d-flex flex-wrap" style="margin: 0 -0.5rem">
+                        <div class="container d-flex flex-wrap">
                             @foreach(\Hyde\Support\Filesystem\MediaFile::all() as $mediaFile)
                                 <div class="col-lg-4 p-2 d-flex flex-grow-1">
                                     <figure class="card w-100 p-2 mb-0">
