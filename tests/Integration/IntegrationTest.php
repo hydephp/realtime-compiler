19dd1d5006273136c7dd477109bcdbb0485a6655
Caen De Silva <caen@desilva.se>
2024-07-15 14:34:09 +0200

    Update test name

--- tests/Integration/IntegrationTest.php
+++ tests/Integration/IntegrationTest.php
@@ -4,7 +4,7 @@ namespace Hyde\RealtimeCompiler\Tests\Integration;
 
 class IntegrationTest extends IntegrationTestCase
 {
-    public function testExample()
+    public function testRealtimeCompiler()
     {
         $this->get('/')
             ->assertStatus(200)
