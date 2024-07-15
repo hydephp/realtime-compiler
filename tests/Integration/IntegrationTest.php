4fa798d60de188fef4d1779b3f51f05fe196c042
Caen De Silva <caen@desilva.se>
2024-07-15 14:37:14 +0200

    Update IntegrationTest.php

--- tests/Integration/IntegrationTest.php
+++ tests/Integration/IntegrationTest.php
@@ -4,10 +4,17 @@ namespace Hyde\RealtimeCompiler\Tests\Integration;
 
 class IntegrationTest extends IntegrationTestCase
 {
-    public function testRealtimeCompiler()
+    public function testWelcome()
     {
         $this->get('/')
             ->assertStatus(200)
             ->assertSeeText("You're running on HydePHP");
+    }
+
+    public function test404()
+    {
+        $this->get('/non-existent-page')
+            ->assertStatus(404)
+            ->assertSeeText('404 - Not Found');
     }
 }
