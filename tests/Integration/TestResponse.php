ddc7ef3f20347657353c362d6bf7cbe074d26f83
Caen De Silva <caen@desilva.se>
2024-07-15 13:42:17 +0200

    Add output buffer

--- tests/Integration/TestResponse.php
+++ tests/Integration/TestResponse.php
@@ -13,7 +13,10 @@ class TestResponse
     public static function get(IntegrationTestCase $test, string $uri): static
     {
         $guzzle = new Client();
+
+        ob_start();
         $response = $guzzle->get('http://localhost:8080'.$uri);
+        ob_end_clean();
 
         return new static($test, $response);
     }
