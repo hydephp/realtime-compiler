7d98f5a48c90f726901527869f63a438f49a6f9c
Emma De Silva <emma@desilva.se>
2026-07-09 19:32:20 +0200

    Fix method visibility

--- src/Http/BaseController.php
+++ src/Http/BaseController.php
@@ -47,7 +47,7 @@ abstract class BaseController
         throw new HttpException($code, $message);
     }
 
-    protected static function matchStatusCode(int $statusCode): string
+    public static function matchStatusCode(int $statusCode): string
     {
         return match ($statusCode) {
             200 => 'OK',
